1e999d9688a180a6bd1af6dc01bab582212334a3
Federico Aragon <federico.aragon@mudafy.com>
2022-05-06 17:59:36 -0300

    First commit to try BigQueryTableExistenceSensor

--- Cloud Composer/dags/dag_get_listed_unlisted_events.py
+++ Cloud Composer/dags/dag_get_listed_unlisted_events.py
@@ -1,7 +1,9 @@
 from airflow import DAG
-from airflow.operators.dummy import DummyOperator 
-
+from airflow.utils.trigger_rule import TriggerRule
+from airflow.operators.dummy import DummyOperator
+from airflow.providers.google.cloud.sensors.bigquery import BigQueryTableExistenceSensor
 from datetime import datetime
+
 from dependencies.keys_and_constants import PROJECT_ID, DATASET_MUDATA_CURATED, TRIGGER_RULES
 
 
@@ -11,15 +13,43 @@ with DAG(
         start_date= datetime(2021, 1, 1),
         catchup= False
     ) as dag:
-    
+
+
+
     task_1 = DummyOperator(
         task_id= 'task_query_posgresql',
-        trigger_rule= TRIGGER_RULES.ALL_SUCCESS
+        trigger_rule= TriggerRule.ALL_SUCCESS
     )
+
 
     task_2 = DummyOperator(
         task_id= 'task_update_table',
-        trigger_rule= TRIGGER_RULES.ALL_SUCCESS
+        trigger_rule= TriggerRule.ALL_SUCCESS
     )
 
     task_1 >> task_2
+
+with DAG(
+    dag_id= 'dag_test',
+    schedule_interval= None,
+    start_date= datetime(2021, 1, 1),
+    catchup= False
+) as dag:
+
+    bigquery_table_existance = BigQueryTableExistenceSensor(
+        project_id= PROJECT_ID, 
+        dataset_id= 'DW_Mudafy',
+        table_id= 'FCT_Busquedas',
+    )
+
+    table_exists = DummyOperator(
+        task_id= 'table_exists',
+        trigger_rule= TriggerRule.ALL_SUCCESS
+    )
+
+    table_doesnt_exists = DummyOperator(
+        task_id= 'table_doesnt_exist',
+        trigger_rule= TriggerRule.ALL_FAILED
+    )
+
+    bigquery_table_existance >> [table_exists, table_doesnt_exists]
