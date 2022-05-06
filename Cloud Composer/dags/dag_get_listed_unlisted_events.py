df66136aff6dad0d2caf3bf4bcc944cdb409e439
Federico Aragon <federico.aragon@mudafy.com>
2022-05-06 16:15:55 -0300

    Business roles constants added

--- Cloud Composer/dags/dag_get_listed_unlisted_events.py
+++ Cloud Composer/dags/dag_get_listed_unlisted_events.py
@@ -9,16 +9,17 @@ with DAG(
         dag_id= 'dag_get_listed_unlisted_events',
         schedule_interval= None,
         start_date= datetime(2021, 1, 1),
+        catchup= False
     ) as dag:
     
     task_1 = DummyOperator(
         task_id= 'task_query_posgresql',
-        # trigger_rule= TRIGGER_RULES.ALL_SUCCESS
+        trigger_rule= TRIGGER_RULES.ALL_SUCCESS
     )
 
     task_2 = DummyOperator(
         task_id= 'task_update_table',
-        # triger_rule= TRIGGER_RULES.ALL_SUCCESS
+        trigger_rule= TRIGGER_RULES.ALL_SUCCESS
     )
 
     task_1 >> task_2
