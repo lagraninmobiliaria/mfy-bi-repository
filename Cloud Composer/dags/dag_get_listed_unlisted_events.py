4ef69334c5065b91a142e94cf25c43fc3c7cfa07
Federico Aragon <federico.aragon@mudafy.com>
2022-05-06 12:45:03 -0300

    Turn off trigger rule for dummy operators

--- Cloud Composer/dags/dag_get_listed_unlisted_events.py
+++ Cloud Composer/dags/dag_get_listed_unlisted_events.py
@@ -13,12 +13,12 @@ with DAG(
     
     task_1 = DummyOperator(
         task_id= 'task_query_posgresql',
-        trigger_rule= TRIGGER_RULES.ALL_SUCCESS
+        # trigger_rule= TRIGGER_RULES.ALL_SUCCESS
     )
 
     task_2 = DummyOperator(
         task_id= 'task_update_table',
-        triger_rule= TRIGGER_RULES.ALL_SUCCESS
+        # triger_rule= TRIGGER_RULES.ALL_SUCCESS
     )
 
     task_1 >> task_2
