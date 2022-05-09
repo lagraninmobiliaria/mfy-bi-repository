ca5cdda8556aa82a6a2e8825422c6b77ddfb68d5
Federico Aragon <federico.aragon@mudafy.com>
2022-05-09 16:12:40 -0300

    Deletion of tables DAG included

--- Cloud Composer/dags/non_scheduled_dags.py
+++ Cloud Composer/dags/non_scheduled_dags.py
@@ -1,7 +1,7 @@
 from airflow import DAG
 from airflow.utils.trigger_rule import TriggerRule
 from airflow.operators.dummy import DummyOperator
-from airflow.providers.google.cloud.operators.bigquery import BigQueryCreateEmptyTableOperator
+from airflow.providers.google.cloud.operators.bigquery import BigQueryCreateEmptyTableOperator, BigQueryDeleteTableOperator
 from airflow.providers.google.cloud.sensors.bigquery import BigQueryTableExistenceSensor
 
 from datetime import datetime
@@ -44,27 +44,37 @@ with DAG(
     start_task >> tasks >> end_task
 
 with DAG(
-    dag_id= 'dag_test',
+    dag_id= 'run_logs_table_deletion',
     schedule_interval= None,
     start_date= datetime(2021, 1, 1),
     catchup= False
 ) as dag_2:
 
-    bigquery_table_existance = BigQueryTableExistenceSensor(
-        task_id= 'bigquery_table_existance',
-        project_id= PROJECT_ID, 
-        dataset_id= 'DW_Mudafy',
-        table_id= 'FCT_Busquedas',
+    start_task_2 = DummyOperator(
+        task_id= 'start',
     )
 
-    table_exists = DummyOperator(
-        task_id= 'table_exists',
-        trigger_rule= TriggerRule.ALL_SUCCESS
+    tasks_2 = []
+    for dataset in [DATASET_MUDATA_RAW, DATASET_MUDATA_CURATED, DATASET_MUDATA_AGGREGATED]:
+        task = BigQueryCreateEmptyTableOperator(
+            task_id= 'table_creation_in_' + dataset,
+            project_id= PROJECT_ID,
+            dataset_id= dataset,
+            table_id= 'dag_runs',
+            trigger_rule= TriggerRule.ALL_SUCCESS,
+            exists_ok= True, 
+            schema_fields= [
+                {"name": "run_id", "type": "INTEGER", "mode": "REQUIRED"},
+                {"name": "date", "type": "DATE", "mode": "REQUIRED"},
+                {"name": "dag_id", "type": "STRING", "mode": "REQUIRED"},
+                {"name": "successful_run", "type": "BOOL", "mode": "REQUIRED"}
+            ]
+        )
+
+        tasks_2.append(task)
+    
+    end_task_2 = DummyOperator(
+        task_id= 'finish'
     )
 
-    table_doesnt_exists = DummyOperator(
-        task_id= 'table_doesnt_exist',
-        trigger_rule= TriggerRule.ALL_FAILED
-    )
-
-    bigquery_table_existance >> [table_exists, table_doesnt_exists]
+    start_task_2 >> tasks_2 >> end_task_2
