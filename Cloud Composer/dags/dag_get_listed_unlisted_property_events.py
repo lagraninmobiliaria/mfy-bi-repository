ceeafa752d1a7bb5415245f1945b6e93d86f54d1
Federico Aragon <federico.aragon@mudafy.com>
2022-05-11 11:25:26 -0300

    DAG naming, removal of trial DAGs and keys and constants modifications

--- Cloud Composer/dags/dag_get_listed_unlisted_property_events.py
+++ Cloud Composer/dags/dag_get_listed_unlisted_property_events.py
@@ -10,10 +10,10 @@ from dependencies.keys_and_constants import PROJECT_ID, DATASET_MUDATA_RAW, EXTE
 from include.sql import queries
 
 with DAG(
-    dag_id= 'get_listed_unlisted_events.py',
-    start_date= datetime(2021, 5, 3),
+    dag_id= 'get_listed_unlisted_property_events.py',
+    start_date= datetime(2021, 5, 3),   
     schedule_interval= '@daily',
-    catchup= True,    
+    catchup= False,    
 ) as dag:
     # This task fetchs the listed and unlisted from pogresql and 
     # it stores them in Google Cloud Storage
@@ -28,17 +28,18 @@ with DAG(
     # This task fetchs the listed and unlisted from Google Cloud Storage and 
     # it stores them in Google BigQuery
     
-    from_gcs_to_bquery = GCSToBigQueryOperator(
-        task_id='from_gcs_to_bquery',
-        bucket= EXTERNAL_DATA_BUCKET,
-        destination_project_dataset_table=f"{DATASET_MUDATA_RAW}.listed_and_unlisted_events",
-        # schema_fields=[
-        #     {'name': 'name', 'type': 'STRING', 'mode': 'NULLABLE'},
-        #     {'name': 'post_abbr', 'type': 'STRING', 'mode': 'NULLABLE'},
-        # ],
-        create_disposition= 'CREATE_IF_NEEDED',  
-        write_disposition='WRITE_APPEND',
-    )
+    # from_gcs_to_bquery = GCSToBigQueryOperator(
+    #     task_id='from_gcs_to_bquery',
+    #     bucket= EXTERNAL_DATA_BUCKET,
+    #     source_objects= '',
+    #     destination_project_dataset_table=f"{DATASET_MUDATA_RAW}.listed_and_unlisted_property_events",
+    #     # schema_fields=[
+    #     #     {'name': 'name', 'type': 'STRING', 'mode': 'NULLABLE'},
+    #     #     {'name': 'post_abbr', 'type': 'STRING', 'mode': 'NULLABLE'},
+    #     # ],
+    #     create_disposition= 'CREATE_IF_NEEDED',  
+    #     write_disposition='WRITE_APPEND',
+    # )
 
 
-    pass
+    from_pgsql_to_gcs
