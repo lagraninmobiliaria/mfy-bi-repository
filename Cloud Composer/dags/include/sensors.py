ad570a0c7294c50b2dc729349e18b7abcbd73c0f
Federico Aragon <federico.aragon@mudafy.com>
2022-05-23 15:52:54 -0300

    First custom sensor added

---
+++ Cloud Composer/dags/include/sensors.py
@@ -0,0 +1,15 @@
+from datetime import datetime
+
+from airflow.sensors.base import BaseSensorOperator
+
+class FirstDAGRunSensor(BaseSensorOperator):
+
+    def __init__(self, *, poke_interval: float = 60, timeout: float = 60 * 60 * 24 * 7, soft_fail: bool = False, mode: str = 'poke', exponential_backoff: bool = False, **kwargs) -> None:
+        
+        super().__init__(poke_interval=poke_interval, timeout= timeout, soft_fail= soft_fail, mode= mode, exponential_backoff=exponential_backoff, **kwargs)
+        self.ds = kwargs['ds']
+        self.dag_start_date = kwargs['dag_start_date']
+
+    def poke(self, **context):
+        return datetime.strptime(self.ds, '%Y-%m-%d').date() == self.dag_start_date.date()
+    
