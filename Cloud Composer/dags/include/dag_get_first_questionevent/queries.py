7a093a8f36dabac909ebd2f42722320f3e84386a
Federico Aragon <federico.aragon@mudafy.com>
2022-05-24 18:46:28 -0300

    Query corrected

--- Cloud Composer/dags/include/dag_get_first_questionevent/queries.py
+++ Cloud Composer/dags/include/dag_get_first_questionevent/queries.py
@@ -5,30 +5,37 @@ from pendulum import datetime
 def get_client_first_questionevent(datetime_floor, datetime_ceil):
    return dedent(
         f'''
-        WITH v_client_first_questionevent AS (
+        SELECT
+            * 
+        FROM EXTERNAL_QUERY(
+            "projects/infrastructure-lgi/locations/us/connections/mudafy", 
+            """
+            WITH v_client_first_questionevent AS (
+                SELECT
+                    ee.client_id        client_id,
+                    MIN(ee.id)          event_id,
+                    MIN(ee.created_at)  event_created_at
+
+                FROM events_questionevent eqe
+                LEFT JOIN events_event ee
+                    ON ee.id = eqe.event_ptr_id
+
+                GROUP BY
+                    ee.client_id
+
+                HAVING 
+                        MIN(ee.created_at) >= TO_TIMESTAMP('{str(datetime_floor).replace('T', ' ')[:-6]}', 'YYYY-MM-DD HH24:MI:SS')
+                    AND MIN(ee.created_at) <  TO_TIMESTAMP('{str(datetime_ceil).replace('T', ' ')[:-6]}', 'YYYY-MM-DD HH24:MI:SS')
+            )
             SELECT
-                ee.client_id        client_id,
-                MIN(ee.id)          event_id,
-                MIN(ee.created_at)  event_created_at
+                v_client_first_questionevent.*                ,
+                ee.opportunity_case_id          opportunity_id
 
-            FROM events_questionevent eqe
-            LEFT JOIN events_event ee
-                ON ee.id = eqe.event_ptr_id
-                
-            GROUP BY 
-                ee.client_id
-
-            HAVING 
-                    MIN(ee.created_at) >= TO_TIMESTAMP('{str(datetime_floor).replace('T', ' ')[:-6]}', 'YYYY-MM-DD HH24:MI:SS')
-                AND MIN(ee.created_at) <  TO_TIMESTAMP('{str(datetime_ceil).replace('T', ' ')[:-6]}', 'YYYY-MM-DD HH24:MI:SS')
-        )
-        SELECT 
-            v_client_first_questionevent.*                ,
-            ee.opportunity_case_id          opportunity_id
-
-        FROM v_client_first_questionevent
-            LEFT JOIN events_event ee
-                ON ee.id = v_client_first_questionevent.event_id
+            FROM v_client_first_questionevent
+                LEFT JOIN events_event ee
+                    ON ee.id = v_client_first_questionevent.event_id
+            """
+        );
         '''
     )
 
