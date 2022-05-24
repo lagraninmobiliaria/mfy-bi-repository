0dc80bfb0a323ab8008c442e74119e5ec9b75987
Federico Aragon <federico.aragon@mudafy.com>
2022-05-24 18:15:35 -0300

    clients first questionevent; queries and DAG first commit

---
+++ Cloud Composer/dags/dag_get_first_questionevent.py
@@ -0,0 +1,51 @@
+from datetime import datetime
+
+from dependencies.keys_and_constants import DATASET_MUDATA_RAW, PROJECT_ID, createDisposition, writeDisposition
+
+from include.dag_get_first_questionevent import queries 
+from airflow                                            import DAG
+from airflow.utils.trigger_rule                         import TriggerRule
+from airflow.operators.dummy                            import DummyOperator
+from airflow.providers.google.cloud.operators.bigquery  import BigQueryInsertJobOperator
+
+with DAG(
+    dag_id= 'get_first_questionevent',
+    schedule_interval= '0/1 0/30 0 ? * * *',
+    start_date= datetime(2020, 4, 8),
+    catchup= True
+) as dag:
+    
+    start_dag = DummyOperator(
+        task_id= 'start_dag'
+    )
+
+    datetime_floor = "{{ data_interval_start }}"
+    datetime_ceil  = "{{ data_interval_end }}"
+    query = queries.get_client_first_questionevent(datetime_floor= datetime_floor, datetime_ceil= datetime_ceil)
+    table_id = 'first_questionevent' 
+
+    query_new_leads = BigQueryInsertJobOperator(
+        configuration= {
+            "query": {
+                "query": query,
+                "useLegacySql": False,
+                "jobReference": {
+                    "projectId": PROJECT_ID,
+                },
+                "destinationTable": {
+                    "projectId": PROJECT_ID,
+                    "datasetId": DATASET_MUDATA_RAW,
+                    "tableId": table_id
+                }, 
+                "writeDisposition": writeDisposition.WRITE_APPEND,
+                "createDisposition": createDisposition.CREATE_IF_NEEDED,
+            }
+        },
+    )
+
+    end_dag = DummyOperator(
+        task_id= 'end_dag',
+        trigger_rule= TriggerRule.ALL_SUCCESS
+    )
+
+    start_dag >> query_new_leads >> end_dag
---
+++ Cloud Composer/dags/include/dag_get_first_questionevent/queries.py
@@ -0,0 +1,38 @@
+from dataclasses import replace
+from textwrap import dedent
+from humanize import deactivate
+from pendulum import datetime
+def get_client_first_questionevent(datetime_floor, datetime_ceil):
+   return dedent(
+        f'''
+        WITH v_client_first_questionevent AS (
+            SELECT
+                ee.client_id        client_id,
+                MIN(ee.id)          event_id,
+                MIN(ee.created_at)  event_created_at
+
+            FROM events_questionevent eqe
+            LEFT JOIN events_event ee
+                ON ee.id = eqe.event_ptr_id
+                
+            GROUP BY 
+                ee.client_id
+
+            HAVING 
+                    MIN(ee.created_at) >= TO_TIMESTAMP('{str(datetime_floor).replace('T', ' ')[:-6]}', 'YYYY-MM-DD HH24:MI:SS')
+                AND MIN(ee.created_at) <  TO_TIMESTAMP('{str(datetime_ceil).replace('T', ' ')[:-6]}', 'YYYY-MM-DD HH24:MI:SS')
+        )
+        SELECT 
+            v_client_first_questionevent.*                ,
+            ee.opportunity_case_id          opportunity_id
+
+        FROM v_client_first_questionevent
+            LEFT JOIN events_event ee
+                ON ee.id = v_client_first_questionevent.event_id
+        '''
+    )
+
+if __name__ == "__main__":
+    datetime_floor = datetime(2021, 6, 27, 17, 0, 0, 0)
+    datetime_ceil = datetime(2021, 6, 27, 17, 5, 0, 0)
+    print(get_client_first_questionevent(datetime_floor, datetime_ceil))
