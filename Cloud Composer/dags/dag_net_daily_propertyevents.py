a31e1d7c2e2b2edacf8640864f84a01a91fbc7dd
Federico Aragon <federico.aragon@mudafy.com>
2022-05-24 11:44:00 -0300

    end_date removed

--- Cloud Composer/dags/dag_net_daily_propertyevents.py
+++ Cloud Composer/dags/dag_net_daily_propertyevents.py
@@ -169,7 +169,6 @@ default_args = {
 with DAG(
     dag_id= 'net_daily_propertyevents',
     start_date= datetime(2021, 5, 3),
-    end_date= datetime(2021, 5, 4),
     schedule_interval= '@daily',
     default_args= default_args,
     catchup= True,
