9c1a0c7dfc6621fd4e27909f540ad5cc4aed7c0e
Federico Aragon <federico.aragon@mudafy.com>
2022-05-26 14:13:54 -0300

    Log data_intervals and types at the very beginning

--- Cloud Composer/dags/dag_get_first_questionevent.py
+++ Cloud Composer/dags/dag_get_first_questionevent.py
@@ -1,11 +1,14 @@
 from datetime import datetime
 
+import py
+
 from dependencies.keys_and_constants import DATASET_MUDATA_RAW, PROJECT_ID, createDisposition, writeDisposition
 
 from include.dag_get_first_questionevent import queries 
 from airflow                                            import DAG
 from airflow.utils.trigger_rule                         import TriggerRule
 from airflow.operators.dummy                            import DummyOperator
+from airflow.operators.python                           import PythonOperator
 from airflow.providers.google.cloud.operators.bigquery  import BigQueryInsertJobOperator
 
 with DAG(
@@ -15,13 +18,21 @@ with DAG(
     end_date= datetime(2020, 4, 9),
     catchup= True
 ) as dag:
+
+    datetime_floor = "{{data_interval_start}}"
+    datetime_ceil  = "{{data_interval_end}}"
     
-    start_dag = DummyOperator(
-        task_id= 'start_dag'
+    start_dag = PythonOperator(
+        task_id= 'start_dag',
+        python_callable= lambda x: print(
+            datetime_floor,
+            type(datetime_floor),
+            datetime_ceil,
+            type(datetime_ceil),
+            sep= '\n'
+        )
     )
 
-    datetime_floor = {{data_interval_start}}
-    datetime_ceil  = {{data_interval_end}}
 
     query = queries.get_client_first_questionevent(datetime_floor, datetime_ceil)
     table_id = 'first_questionevent' 
