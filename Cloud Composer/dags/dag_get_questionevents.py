752cacbe13f08807920202cd68172f46f96f18bf
Federico Aragon <federico.aragon@mudafy.com>
2022-05-27 15:45:14 -0300

    Include destination table and SQL_QUERY_PATH included as a variable

--- Cloud Composer/dags/dag_get_questionevents.py
+++ Cloud Composer/dags/dag_get_questionevents.py
@@ -1,7 +1,8 @@
 from datetime import datetime
 
+from dependencies.keys_and_constants import DATASET_MUDATA_RAW, PROJECT_ID, writeDisposition, createDisposition
+
 from airflow import DAG
-from airflow.operators.python                           import PythonOperator
 from airflow.operators.dummy                            import DummyOperator
 from airflow.operators.bash                             import BashOperator
 from airflow.providers.google.cloud.operators.bigquery  import BigQueryInsertJobOperator
@@ -10,7 +11,7 @@ with DAG(
     dag_id= 'get_questionevents',
     schedule_interval= '@daily',
     start_date= datetime(2020, 4, 8),
-    end_date= datetime(2020, 4, 9),
+    max_active_runs= 5, 
     is_paused_upon_creation= True,
 ) as dag:
 
@@ -21,12 +22,25 @@ with DAG(
         bash_command= f"echo Start DAGRun - Logical date: {date}"
     )
 
+    table_id = 'questionevents'
+    SQL_QUERY_PATH= './include/dag_get_questionevents/queries/get_questionevents.sql'
+
     task_get_questionevents = BigQueryInsertJobOperator(
         task_id= 'get_questionevents',
         configuration= {
             "query": {
-                "query": "{% include './include/dag_get_questionevents/queries/get_questionevents.sql' %}",
+                "query": f"{'{%'} include {SQL_QUERY_PATH} {'%}'}",
                 "useLegacySql": False,
+                 "jobReference": {
+                    "projectId": PROJECT_ID,
+                },
+                "destinationTable": {
+                    "projectId": PROJECT_ID,
+                    "datasetId": DATASET_MUDATA_RAW,
+                    "tableId": table_id
+                }, 
+                "writeDisposition": writeDisposition.WRITE_APPEND,
+                "createDisposition": createDisposition.CREATE_IF_NEEDED,
             }
         }
     )
