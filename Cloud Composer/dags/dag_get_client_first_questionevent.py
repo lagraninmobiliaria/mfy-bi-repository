f7d1ab22bdb8ff67792b84ab71779477e7a70203
Federico Aragon <federico.aragon@mudafy.com>
2022-05-30 14:12:39 -0300

    'get_client_first_questionevent' design

--- Cloud Composer/dags/dag_get_client_first_questionevent.py
+++ Cloud Composer/dags/dag_get_client_first_questionevent.py
@@ -1,12 +1,17 @@
 from datetime import datetime
-
-from dependencies.keys_and_constants import DATASET_MUDATA_RAW, PROJECT_ID, createDisposition, writeDisposition
  
 from airflow                                            import DAG
 
 from airflow.utils.trigger_rule                         import TriggerRule
 from airflow.operators.dummy                            import DummyOperator
-from airflow.providers.google.cloud.operators.bigquery  import BigQueryInsertJobOperator
+from airflow.operators.python                           import BranchPythonOperator
+
+def is_first_run(**context):
+    prev_ds = context.get('prev_ds')
+    if prev_ds is None:
+        return 'task_branch_a'
+    else:
+        return 'task_branch_b'
 
 with DAG(
     dag_id= 'get_client_first_questionevent',
@@ -20,36 +25,23 @@ with DAG(
     start_dag = DummyOperator(
         task_id= 'start_dag',
     )
-    data_interval_start= "{{data_interval_start}}"
-    data_interval_end= "{{data_interval_end}}"
+    
+    branch_task = BranchPythonOperator(
+        task_id= 'task_is_first_run',
+        python_callable= is_first_run,
+    ) 
 
-    table_id = 'client_first_questionevent' 
-
-    query_new_questionevents = BigQueryInsertJobOperator(
-        gcp_conn_id= "bigquery_default",
-        location= 'us',
-        task_id= 'query_new_questionevents',
-        configuration= {
-            "query": {
-                # "query": query,
-                "useLegacySql": False,
-                "jobReference": {
-                    "projectId": PROJECT_ID,
-                },
-                "destinationTable": {
-                    "projectId": PROJECT_ID,
-                    "datasetId": DATASET_MUDATA_RAW,
-                    "tableId": table_id
-                }, 
-                "writeDisposition": writeDisposition.WRITE_APPEND,
-                "createDisposition": createDisposition.CREATE_IF_NEEDED,
-            }
-        },
+    task_branch_a = DummyOperator(
+        task_id= 'branch_a'
     )
+    task_branch_b = DummyOperator(
+        task_id= 'branch_b'
+    )
+
 
     end_dag = DummyOperator(
         task_id= 'end_dag',
         trigger_rule= TriggerRule.ALL_SUCCESS
     )
 
-    start_dag >> query_new_questionevents >> end_dag
+    start_dag >> [task_branch_a, task_branch_b] >> end_dag
