b1106c35ed53a2b2b76f67574cb28f8819b81611
Federico Aragon <federico.aragon@mudafy.com>
2022-05-30 18:17:29 -0300

    'get_client_first_questionevent' query changed

--- Cloud Composer/dags/include/dag_get_client_first_questionevent/queries/get_client_first_questionevent.sql
+++ Cloud Composer/dags/include/dag_get_client_first_questionevent/queries/get_client_first_questionevent.sql
@@ -1,18 +1,32 @@
-SELECT  
-    MIN(questionevents.event_id)        event_id,
-    MIN(questionevents.created_at)      created_at,
-    questionevents.client_id            client_id,
-    MIN(questionevents.opportunity_id)  opportunity_id
-    
-FROM `infrastructure-lgi.{{DATASET_MUDATA_RAW}}.questionevents` questionevents
-WHERE  
-    NOT EXISTS(
-        SELECT client_id
-        FROM `infrastructure-lgi.{{DATASET_MUDATA_CURATED}}.clients_first_questionevent` clients_first_questionevent
-        WHERE questionevents.client_id = clients_first_questionevent.client_id
-    )
-    AND questionevents.created_at >= TIMESTAMP('{{ data_interval_start }}')
-    AND questionevents.created_at <  TIMESTAMP('{{ data_interval_end }}')
+SELECT
+    questionevents.event_id                             event_id,
+    questionevents.created_at                           created_at,
+    questionevents.client_id                            client_id,
+    questionevents.opportunity_id                       opportunity_id
 
-GROUP BY
-    questionevents.client_id
+FROM (
+    SELECT  
+        questionevents.event_id                         event_id,
+        questionevents.created_at                       created_at,
+        questionevents.client_id                        client_id,
+        questionevents.opportunity_id                   opportunity_id,
+        ROW_NUMBER() OVER(
+            PARTITION BY questionevents.client_id 
+            ORDER BY questionevents.created_at ASC
+        )                                               created_order_n
+
+    FROM `infrastructure-lgi.{{DATASET_MUDATA_RAW}}.questionevents` questionevents
+    WHERE  
+        NOT EXISTS(
+            SELECT 
+                *
+            FROM `infrastructure-lgi.{{DATASET_MUDATA_CURATED}}.clients_first_questionevent` clients_first_questionevent
+            WHERE 
+                questionevents.client_id = clients_first_questionevent.client_id
+        )
+        AND questionevents.created_at >= TIMESTAMP('{{ data_interval_start }}')
+        AND questionevents.created_at <  TIMESTAMP('{{ data_interval_end }}')
+) time_range_questionevents
+
+WHERE 
+    time_range_questionevents.created_order_n = 1
