ce0e9331197f813afba1b1d7c47e940c41ae78fc
Federico Aragon <federico.aragon@mudafy.com>
2022-06-07 15:53:27 -0300

    Log job_id to understand why is it failing

--- Cloud Composer/dags/include/dag_get_client_reactivation_events/functions.py
+++ Cloud Composer/dags/include/dag_get_client_reactivation_events/functions.py
@@ -4,7 +4,10 @@ from dependencies.keys_and_constants import PROJECT_ID
 
 def get_clients_with_search_reactivation_event(**context):
     bq_client= bigquery.Client(project= PROJECT_ID)
-    bq_job= bq_client.get_job(job_id= context['task_instance'].xcom_pull('query_daily_search_reactivation_events'))
+    job_id= context['task_instance'].xcom_pull('query_daily_search_reactivation_events')
+    print(job_id)
+    bq_job= bq_client.get_job(job_id= job_id)
     query_results= bq_job.to_dataframe()
     output = list(query_results.client_id.unique())
+    print(output)
     context['task_instance'].xcom_push(key= 'list_client_id', value= output)
