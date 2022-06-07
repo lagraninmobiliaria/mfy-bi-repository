8ec40d9b3ec4c42073864cd2278968a2cc38b9ca
Federico Aragon <federico.aragon@mudafy.com>
2022-06-07 13:09:14 -0300

    missing inverted commas in queries

--- Cloud Composer/dags/include/dag_get_client_reactivation_events/queries/daily_search_reactivation_events.sql
+++ Cloud Composer/dags/include/dag_get_client_reactivation_events/queries/daily_search_reactivation_events.sql
@@ -2,5 +2,5 @@ SELECT
     *
 FROM `{{ params.project_id }}.{{ params.mudata_raw }}.search_reactivation_events` search_reactivation_events
 WHERE
-        search_reactivation_events.created_at >= TIMESTAMP({{ data_interval_start }})
-    AND search_reactivation_events.created_at <  TIMESTAMP({{ data_interval_end }})
+        search_reactivation_events.created_at >= TIMESTAMP('{{ data_interval_start }}')
+    AND search_reactivation_events.created_at <  TIMESTAMP('{{ data_interval_end }}')
