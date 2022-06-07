6fccec48276594fc6deaf5c385ccccd5af9ac12b
Federico Aragon <federico.aragon@mudafy.com>
2022-06-07 18:38:13 -0300

    task_id to get the job_id incorrectly setted

--- Cloud Composer/dags/include/dag_get_client_reactivation_events/functions.py
+++ Cloud Composer/dags/include/dag_get_client_reactivation_events/functions.py
@@ -5,7 +5,7 @@ from dependencies.keys_and_constants import PROJECT_ID
 def validate_search_reactivation_as_client_reactivation(**context):
     bq_client= Client(project= PROJECT_ID, location= 'us-central1')
     
-    job_id= context['task_instance'].xcom_pull('create_client_reactivation_events_table')
+    job_id= context['task_instance'].xcom_pull('query_daily_search_reactivation_events')
     bq_job= bq_client.get_job(job_id= job_id)
     query_results= bq_job.to_dataframe()
 
