40cce38c78529746d26f9d7c801dae158c0f67e0
Federico Aragon <federico.aragon@mudafy.com>
2022-06-08 10:46:51 -0300

    Query modified

--- Cloud Composer/dags/include/dag_get_client_reactivation_events/queries/client_last_closed_event.sql
+++ Cloud Composer/dags/include/dag_get_client_reactivation_events/queries/client_last_closed_event.sql
@@ -1,6 +1,12 @@
 SELECT
-    *
+    closed_client_events.client_id          client_id,
+    MAX(closed_client_events.event_id)      event_id,
+    MAX(closed_client_events.created_at)    created_at
+
 FROM `{{ params.project_id }}.{{ params.mudata_raw }}.closed_client_events` closed_client_events
 WHERE
         closed_client_events.client_id  = {}
     AND closed_client_events.created_at < TIMESTAMP('{}')
+
+GROUP BY 
+    closed_client.client_id
