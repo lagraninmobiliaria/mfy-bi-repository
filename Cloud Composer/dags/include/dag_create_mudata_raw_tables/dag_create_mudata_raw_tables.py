550c12a004cdf7195f3267e12a0759cde2f2204d
Federico Aragon <federico.aragon@mudafy.com>
2022-06-09 16:25:38 -0300

    Create DW Tables for mudata_raw dataset DAG created

---
+++ Cloud Composer/dags/include/dag_create_mudata_raw_tables/dag_create_mudata_raw_tables.py
@@ -0,0 +1,44 @@
+from dependencies.keys_and_constants import DATASET_MUDATA_RAW, PROJECT_ID
+from table_schemas import *
+
+from airflow import DAG
+from airflow.operators.dummy import DummyOperator
+from airflow.providers.google.cloud.operators.bigquery import BigQueryCreateEmptyTableOperator
+
+tables_to_create= [
+    CLIENT_CREATION, CLIENT_MAIL_SUBSCRIPTION, 
+    BUYING_OPPORTUNITY_CASE, USER_ASSIGNMENT,
+    TASKS, TICKETS
+]
+
+with DAG(
+    dag_id= "mudata_raw_tables_creation",
+    schedule_interval= None,
+    is_paused_upon_creation= True,
+) as dag:
+
+    task_start_dag = DummyOperator(
+        task_id= "start_dag"
+    )
+
+    tasks_create_tables = []
+
+    for table in tables_to_create:
+
+        task = BigQueryCreateEmptyTableOperator(
+            task_id= f"create_table_{table.get('table_id')}",
+            project_id= PROJECT_ID,
+            dataset_id= DATASET_MUDATA_RAW,
+            table_id= table.get("table_id"),
+            schema_fields= table.get("schema_fields"),
+            time_partitioning= table.get("time_partitioning"),
+            exists_ok= True,
+        )
+
+        tasks_create_tables.append(task)
+
+    task_end_dag = DummyOperator(
+        task_id= "end_dag"
+    )
+
+    task_start_dag >> tasks_create_tables >> task_end_dag
---
+++ Cloud Composer/dags/include/dag_create_mudata_raw_tables/table_schemas.py
@@ -0,0 +1,122 @@
+from dependencies.keys_and_constants import schemaTypes
+from google.cloud.bigquery.table import TimePartitioning, TimePartitioningType
+CLIENT_CREATION = {
+    "table_id": "client_cration",
+    "schema_fields": [
+        {"name": "registered_date", "type": schemaTypes.DATETIME},
+        {"name": "client_id", "type": schemaTypes.INTEGER},
+        {"name": "assignee_id","type": schemaTypes.INTEGER},
+        {"name": "name","type": schemaTypes.STRING},
+        {"name": "phone","type": schemaTypes.STRING},
+        {"name": "email","type": schemaTypes.STRING},
+        {"name": "source","type": schemaTypes.STRING},
+        {"name": "country","type": schemaTypes.STRING},
+        {"name": "creation_date","type": schemaTypes.DATETIME},
+    ],
+    "time_partitioning": TimePartitioning(
+        field= "registered_date", 
+        type_= TimePartitioningType.DAY 
+    ),
+}
+
+CLIENT_MAIL_SUBSCRIPTION = {
+    "table_id": "client_mail_subscription",
+    "schema_fields": [
+        {"name": "registered_date", "type": schemaTypes.DATETIME},
+        {"name": "client_id", "type": schemaTypes.INTEGER},
+        {"name": "subscription_date", "type": schemaTypes.DATETIME},
+        {"name": "condition", "type": schemaTypes.STRING}
+    ],
+    "time_partitioning": TimePartitioning(
+        field= "registered_date", 
+        type_= TimePartitioningType.DAY 
+    ),
+}
+
+BUYING_OPPORTUNITY_CASE = {
+    "table_id": "buying_opportunity_case",
+    "schema_fields": [
+        {"name": "registered_date", "type": schemaTypes.DATETIME},
+        {"name": "event_id", "type": schemaTypes.INTEGER},
+        {"name": "opportunity_id", "type": schemaTypes.INTEGER},
+        {"name": "client_id", "type": schemaTypes.INTEGER},
+        {"name": "ticket_id", "type": schemaTypes.INTEGER},
+        {"name": "property_id", "type": schemaTypes.INTEGER},
+        {"name": "develompent_id", "type": schemaTypes.INTEGER},
+        {"name": "source", "type": schemaTypes.STRING},
+        {"name": "reference", "type": schemaTypes.STRING},
+        {"name": "is_new", "type": schemaTypes.BOOL}
+    ],
+    "time_partitioning": TimePartitioning(
+        field= "registered_date", 
+        type_= TimePartitioningType.DAY 
+    ),
+}
+
+TASKS = {
+    "table_id": "tasks",
+    "schema_fields": [
+        {"name": "registered_date", "type": schemaTypes.DATETIME},
+        {"name":"task_id", "type": schemaTypes.INTEGER},
+        {"name":"creation_date", "type": schemaTypes.DATETIME},
+        {"name":"deadline_date", "type": schemaTypes.DATETIME},
+        {"name":"completion_date", "type": schemaTypes.DATETIME},
+        {"name":"title", "type": schemaTypes.STRING},
+        {"name":"description", "type": schemaTypes.STRING},
+        {"name":"assignee_id", "type": schemaTypes.INTEGER},
+        {"name":"client_id", "type":schemaTypes.INTEGER},
+        {"name":"property_id", "type":schemaTypes.INTEGER},
+        {"name":"kind", "type": schemaTypes.STRING},
+        {"name":"completed_by_id", "type":schemaTypes.INTEGER},
+        {"name":"created_by_id", "type":schemaTypes.INTEGER},
+        {"name":"deadline_type", "type": schemaTypes.STRING},
+        {"name":"develompent_id", "type": schemaTypes.INTEGER},
+        {"name":"opportunity_id", "type": schemaTypes.INTEGER},
+        {"name":"is_completed", "type": schemaTypes.BOOL},
+        {"name":"status", "type": schemaTypes.STRING},
+        {"name":"process_step_id", "type": schemaTypes.INTEGER},
+        {"name":"last_modified_at", "type": schemaTypes.DATETIME},
+        {"name":"next_priority_check", "type": schemaTypes.DATETIME},
+        {"name":"sla", "type": schemaTypes.DATETIME},
+        {"name":"booking_id", "type": schemaTypes.INTEGER}
+    ]
+}
+
+USER_ASSIGNMENT = {
+    "table_id": "user_assignment",
+    "schema_fields": [
+        {"name": "registered_date", "type": schemaTypes.DATETIME},
+        {"name": "event_id", "type": schemaTypes.INTEGER},
+        {"name": "client_id", "type": schemaTypes.INTEGER},
+        {"name": "ticket_id", "type": schemaTypes.INTEGER},
+        {"name": "new_owner_id", "type": schemaTypes.INTEGER},
+        {"name": "old_owner_id", "type": schemaTypes.INTEGER},
+        {"name": "from_datetime_z", "type": schemaTypes.DATETIME},
+        {"name": "to_datetime_z", "type": schemaTypes.DATETIME},
+    ],
+    "time_partitioning": TimePartitioning(
+        field= "registered_date", 
+        type_= TimePartitioningType.DAY 
+    ),
+}
+
+TICKETS = {
+    "table_id": "tickets",
+    "schema_fields": [
+        {"name": "registered_date", "type": schemaTypes.DATETIME},
+        {"name": "ticket_id", "type": schemaTypes.INTEGER},
+        {"name": "client_id", "type": schemaTypes.INTEGER},
+        {"name": "country_id", "type": schemaTypes.INTEGER},
+        {"name": "opportunity_id", "type": schemaTypes.INTEGER},
+        {"name": "user_id", "type": schemaTypes.INTEGER},
+        {"name": "created_datetime_z", "type": schemaTypes.DATETIME},
+        {"name": "last_update_datetime_z", "type": schemaTypes.DATETIME},
+        {"name": "last_update_by", "type": schemaTypes.INTEGER},
+        {"name": "status", "type": schemaTypes.STRING},
+        {"name": "is_new", "type": schemaTypes.BOOL},
+    ],
+    "time_partitioning": TimePartitioning(
+        field= "registered_date", 
+        type_= TimePartitioningType.DAY 
+    ),
+}
