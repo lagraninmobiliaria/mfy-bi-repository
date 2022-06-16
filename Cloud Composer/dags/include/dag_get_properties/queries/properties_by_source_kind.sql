583a556549aa44bad7d8d6baca66c2ff3eda01d7
Federico Aragon <federico.aragon@mudafy.com>
2022-06-16 14:56:15 -0300

    query modified

--- Cloud Composer/dags/include/dag_get_properties/queries/properties_by_source_kind.sql
+++ Cloud Composer/dags/include/dag_get_properties/queries/properties_by_source_kind.sql
@@ -1,5 +1,12 @@
-SELECT
+SELECT 
     *
-FROM properties_property pp
-WHERE
-    pp.source_kind = {}
+FROM EXTERNAL_QUERY(
+    "projects/infrastructure-lgi/locations/us-central1/connections/mudafy-prod-replic-us-central",
+    """
+    SELECT
+        *
+    FROM properties_property pp
+    WHERE
+        pp.source_kind = {}
+    """
+)
