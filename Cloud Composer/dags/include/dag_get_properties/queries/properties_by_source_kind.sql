81a2604c79f14d2b4121aee400c58bc6122236fb
Federico Aragon <federico.aragon@mudafy.com>
2022-06-16 14:17:33 -0300

    get_properties_no_creation_date created

---
+++ Cloud Composer/dags/include/dag_get_properties/queries/properties_by_source_kind.sql
@@ -0,0 +1,5 @@
+SELECT
+    *
+FROM properties_property pp
+WHERE
+    pp.source_kind = {}
---
+++ Cloud Composer/dags/include/dag_get_properties/stg_get_properties_no_creation_date.py
@@ -0,0 +1,58 @@
+from dependencies.keys_and_constants import PROJECT_ID, STG_DATASET_MUDATA_RAW
+
+from airflow import DAG
+from airflow.operators.dummy import DummyOperator
+from airflow.providers.google.cloud.operators.bigquery import BigQueryInsertJobOperator
+
+from google.cloud.bigquery import QueryJobConfig, WriteDisposition, CreateDisposition, QueryJob
+
+source_kinds = [
+    'stock', 'manual', 
+    'external_feed', 'alliance'
+]
+
+with DAG(
+    dag_id= 'stg_get_properties_no_creation_date',
+    schedule_interval= None,
+    start_date= (2021, 31, 8)
+) as dag:
+
+    task_start_dag = DummyOperator(
+        task_id= 'start_dag'
+    )
+
+    table_id= 'properties'
+    tasks= []
+    for source_kind in source_kinds:
+        with open('./queries/properties_by_source_kind.sql') as sql:
+            query = sql.read().format(source_kind)
+    
+        task = BigQueryInsertJobOperator(
+            task_id= f'get_{source_kind}_properties',
+            configuration= {
+                "query": {
+                    "query": query,
+                    "useLegacySql": False,
+                    "jobReference": {
+                        "projectId": PROJECT_ID,
+                    },
+                    # "destinationTable": {
+                    #     "projectId": PROJECT_ID,
+                    #     "datasetId": STG_DATASET_MUDATA_RAW,
+                    #     "tableId": table_id
+                    # # }, 
+                    # "writeDisposition": WriteDisposition.WRITE_APPEND,
+                    # "createDisposition": CreateDisposition.CREATE_NEVER,
+                }
+            }
+        )
+
+
+        tasks.append(task)
+        
+
+    task_end_dag = DummyOperator(
+        task_id= 'end_dag'
+    )
+
+    task_start_dag >> tasks >> task_end_dag
