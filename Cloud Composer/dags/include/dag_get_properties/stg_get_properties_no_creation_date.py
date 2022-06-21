cca4a0e90795065a899fa05f8242a8aee4273874
Federico Aragon <federico.aragon@mudafy.com>
2022-06-21 14:27:06 -0300

    DAG paused upon creation and task for table creation added

--- Cloud Composer/dags/include/dag_get_properties/stg_get_properties_no_creation_date.py
+++ Cloud Composer/dags/include/dag_get_properties/stg_get_properties_no_creation_date.py
@@ -5,9 +5,9 @@ from dependencies.keys_and_constants import PROJECT_ID, STG_DATASET_MUDATA_RAW
 
 from airflow import DAG
 from airflow.operators.dummy import DummyOperator
-from airflow.providers.google.cloud.operators.bigquery import BigQueryInsertJobOperator
+from airflow.providers.google.cloud.operators.bigquery import BigQueryInsertJobOperator, BigQueryCreateEmptyTableOperator, BigQueryDeleteTableOperator
 
-from google.cloud.bigquery import QueryJobConfig, WriteDisposition, CreateDisposition, QueryJob
+from google.cloud.bigquery import QueryJobConfig, WriteDisposition, CreateDisposition, QueryJob, SchemaUpdateOption
 
 source_kinds = [
     'stock', 'manual', 
@@ -17,7 +17,8 @@ source_kinds = [
 with DAG(
     dag_id= 'stg_get_properties_no_creation_date',
     schedule_interval= None,
-    start_date= datetime(2021, 8, 31)
+    start_date= datetime(2021, 8, 31),
+    is_paused_upon_creation= True,
 ) as dag:
 
     task_start_dag = DummyOperator(
@@ -25,12 +26,17 @@ with DAG(
     )
 
     table_id= 'properties'
-    tasks= []
-    query_path = os.path.join(
-        os.path.dirname(__file__),
-        'queries',
-        'properties_by_source_kind.sql'
+
+    task_create_table = BigQueryCreateEmptyTableOperator(
+        task_id= "create_table",
+        project_id= PROJECT_ID, 
+        dataset_id= STG_DATASET_MUDATA_RAW,
+        table_id= table_id,
     )
+
+    tasks= []
+    query_path = os.path.join(os.path.dirname(__file__),'queries','properties_by_source_kind.sql')
+
     for source_kind in source_kinds:
         with open(query_path, 'r') as sql:
             query = sql.read().format(source_kind)
@@ -43,14 +49,7 @@ with DAG(
                     "useLegacySql": False,
                     "jobReference": {
                         "projectId": PROJECT_ID,
-                    },
-                    # "destinationTable": {
-                    #     "projectId": PROJECT_ID,
-                    #     "datasetId": STG_DATASET_MUDATA_RAW,
-                    #     "tableId": table_id
-                    # }, 
-                    # "writeDisposition": WriteDisposition.WRITE_APPEND,
-                    # "createDisposition": CreateDisposition.CREATE_NEVER,
+                    }
                 }
             }
         )
