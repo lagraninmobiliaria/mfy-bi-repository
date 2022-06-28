ed176828e044b4bdcac7e5ae1c3621e7f55641ac
Federico Aragon <federico.aragon@mudafy.com>
2022-06-28 14:22:50 -0300

    LoadJob included in coad

--- Cloud Composer/dags/include/dag_get_clients_info_on_creation/functions.py
+++ Cloud Composer/dags/include/dag_get_clients_info_on_creation/functions.py
@@ -22,19 +22,15 @@ def client_exists_already(bq_client: Client, client_id, **context):
 
     return exists
 
-def get_clients_data(**context):
+def new_clients_to_load(df_clients_with_first_qe: DataFrame, bq_client: Client, **context) -> list:
+    
+    new_client_rows_to_load= []
 
-    bq_client = Client(project= context['params'].get('project_id'))
-    job_id= context['task_instance'].xcom_pull(task_ids= 'query_first_client_question_events_of_day')
-    bq_job= bq_client.get_job(job_id= job_id, location= 'us-central1')
-
-    query_results_df = bq_job.to_dataframe()
-
-    for i in range(query_results_df.shape[0]):
-        client_id= query_results_df.iloc[i].client_id
+    for i in range(df_clients_with_first_qe.shape[0]):
+        client_id= df_clients_with_first_qe.iloc[i].client_id
 
         if not client_exists_already(bq_client= bq_client, client_id= client_id, **context):
-            opportunity_id= query_results_df.iloc[i].opportunity_id
+            opportunity_id= df_clients_with_first_qe.iloc[i].opportunity_id
 
             client_information_query_path= os.path.join(os.path.dirname(__file__), 'queries', 'get_client_data_ps.sql')
 
@@ -48,11 +44,30 @@ def get_clients_data(**context):
 
             if client_information_results is not None:
                 client_information_results['client_id']= client_id
-                client_information_results['creation_datetime_z']= query_results_df.iloc[i].created_at
+                client_information_results['creation_datetime_z']= df_clients_with_first_qe.iloc[i].created_at
                 client_information_results['registered_datetime_z']= context['data_interval_start']
 
-                print(DataFrame(
-                    data= [client_information_results]
-                ).info())
-        else:
-            print("Client already exists")
+                new_client_rows_to_load.append(client_information_results)
+    
+    return new_client_rows_to_load
+    
+def get_clients_data(**context):
+
+    bq_client = Client(project= context['params'].get('project_id'))
+    job_id= context['task_instance'].xcom_pull(task_ids= 'query_first_client_question_events_of_day')
+    bq_job= bq_client.get_job(job_id= job_id, location= 'us-central1')
+
+    query_results_df = bq_job.to_dataframe()
+    rows_to_load= new_clients_to_load(query_results_df, bq_client, **context)
+
+    if len(rows_to_load):
+        load_job= bq_client.load_table_from_dataframe(
+            dataframe= DataFrame(
+                data= rows_to_load
+            ),
+            destination= f"{context['params'].get('project_id')}.{context['params'].get('mudata_raw')}.clients_creation",
+        )
+
+        return load_job.job_id
+
+
