f4371a0cd0fd654df00380ea694e289f02f7618b
Federico Aragon <federico.aragon@mudafy.com>
2022-06-28 16:43:18 -0300

    dag_create_mudata_curated_tables divided between develop and production

--- Cloud Composer/dags/include/dag_create_mudata_curated_tables/stg_create_mudata_curated_tables.py
+++ Cloud Composer/dags/include/dag_create_mudata_curated_tables/stg_create_mudata_curated_tables.py
@@ -1,7 +1,7 @@
 from datetime import datetime
 
 from include.dag_create_mudata_curated_tables.table_schemas import *
-from dependencies.keys_and_constants import PROJECT_ID, DATASET_MUDATA_CURATED
+from dependencies.keys_and_constants import PROJECT_ID, STG_DATASET_MUDATA_CURATED
 
 from airflow                                            import DAG
 from airflow.operators.dummy                            import DummyOperator
@@ -16,10 +16,14 @@ tables_to_create = [
 ]
 
 with DAG(
-    dag_id= "create_mudata_curated_tables",
+    dag_id= "stg_create_mudata_curated_tables",
     schedule_interval= None,
     start_date= datetime(2021, 1, 1),
-    is_paused_upon_creation= True
+    is_paused_upon_creation= True,
+    params= {
+        'project_id': PROJECT_ID,
+        'mudata_raw': STG_DATASET_MUDATA_CURATED
+    }
 ) as dag:
     
     task_start_dag = DummyOperator(
@@ -31,8 +35,8 @@ with DAG(
     for table_to_create in tables_to_create:
         task_create_table = BigQueryCreateEmptyTableOperator(
             task_id= f"create_table_{table_to_create.get('table_id')}",
-            project_id= PROJECT_ID,
-            dataset_id= DATASET_MUDATA_CURATED,
+            project_id= "{{ params.project_id }}",
+            dataset_id= "{{ params.mudata_raw }}",
             table_id= table_to_create.get("table_id"),
             schema_fields= table_to_create.get("schema_fields"),
             time_partitioning= table_to_create.get("time_partitioning"),
