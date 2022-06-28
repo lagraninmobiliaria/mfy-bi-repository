b421ddfb15f271ce8dc01fea74049b815e62ee74
Federico Aragon <federico.aragon@mudafy.com>
2022-06-28 18:21:54 -0300

    Get question events for Production datasets

---
+++ Cloud Composer/dags/include/dag_get_question_events/prod_get_question_events.py
@@ -0,0 +1,80 @@
+from datetime import datetime
+
+from dependencies.keys_and_constants                    import PROD_DATASET_MUDATA_RAW, PROJECT_ID, schemaTypes
+
+from google.cloud.bigquery                              import WriteDisposition, CreateDisposition
+from google.cloud.bigquery.table                        import TimePartitioningType
+
+from airflow                                            import DAG
+from airflow.utils.trigger_rule                         import TriggerRule
+from airflow.operators.dummy                            import DummyOperator
+from airflow.operators.bash                             import BashOperator
+from airflow.providers.google.cloud.operators.bigquery  import BigQueryInsertJobOperator, BigQueryCreateEmptyTableOperator
+
+with DAG(
+    dag_id= 'prod_get_question_events',
+    schedule_interval= '@daily',
+    start_date= datetime(2020, 4, 13),
+    max_active_runs= 5, 
+    is_paused_upon_creation= True,
+    params= {
+        "polymorphic_ctype_id": 42,
+        'project_id': PROJECT_ID,
+        'mudata_raw': PROD_DATASET_MUDATA_RAW
+    },
+    catchup= True,
+) as dag:
+
+    task_start_dag = BashOperator(
+        task_id = 'start_dag',
+        bash_command= "echo Start DAGRun - Logical date: {{ data_interval_start.date() }}"
+    )
+
+    SQL_QUERY_PATH= f'./queries/{dag.dag_id.split(maxsplit=1, sep="_")[-1]}.sql'
+    table_id = 'question_events'
+
+    task_create_question_events_table = BigQueryCreateEmptyTableOperator(
+        task_id= "create_question_events_table",
+        trigger_rule= TriggerRule.ALL_SUCCESS,
+        project_id= "{{ params.project_id }}",
+        dataset_id= "{{ params.mudata_raw }}",
+        table_id= table_id,
+        schema_fields= [
+            {"name": "event_id", "type": schemaTypes.INTEGER},
+            {"name": "created_at", "type": schemaTypes.TIMESTAMP},
+            {"name": "client_id", "type": schemaTypes.INTEGER},
+            {"name": "opportunity_id", "type": schemaTypes.INTEGER},
+        ],
+        time_partitioning= {
+            "field": "created_at",
+            "type": TimePartitioningType.DAY
+        },
+        cluster_fields= ['client_id'],
+        exists_ok= True
+    )
+
+    task_get_question_events = BigQueryInsertJobOperator(
+        task_id= 'get_question_events',
+        configuration= {
+            "query": {
+                "query": f"{'{%'} include '{SQL_QUERY_PATH}' {'%}'}",
+                "useLegacySql": False,
+                 "jobReference": {
+                    "projectId": "{{ params.project_id }}",
+                },
+                "destinationTable": {
+                    "projectId": "{{ params.project_id }}",
+                    "datasetId": "{{ params.mudata_raw }}",
+                    "tableId": table_id
+                }, 
+                "writeDisposition": WriteDisposition.WRITE_APPEND,
+                "createDisposition": CreateDisposition.CREATE_NEVER,
+            }
+        }
+    )
+
+    task_end_dag = DummyOperator(
+        task_id= 'end_dag'
+    )
+
+    task_start_dag >> task_create_question_events_table >> task_get_question_events >> task_end_dag
