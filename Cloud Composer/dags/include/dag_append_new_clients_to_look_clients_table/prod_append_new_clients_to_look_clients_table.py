7f47e27c9375b1ac5ecb471548f3c92d9a37752c
Federico Aragon <federico.aragon@mudafy.com>
2022-06-30 14:38:48 -0300

    DAG to fill 'look_clients' in prod_mudata_curated created

---
+++ Cloud Composer/dags/include/dag_append_new_clients_to_look_clients_table/prod_append_new_clients_to_look_clients_table.py
@@ -0,0 +1,57 @@
+from datetime import datetime
+
+from dependencies.keys_and_constants import PROJECT_ID, PROD_DATASET_MUDATA_RAW, PROD_DATASET_MUDATA_CURATED
+
+from airflow                                            import DAG
+from airflow.operators.dummy                            import DummyOperator
+from airflow.providers.google.cloud.operators.bigquery  import BigQueryInsertJobOperator
+
+from google.cloud.bigquery import WriteDisposition, CreateDisposition
+
+with DAG(
+    dag_id= 'prod_append_new_clients_to_look_clients_table',
+    schedule_interval= '@daily',
+    start_date= datetime(2020, 4, 13, 1, 30),
+    max_active_runs=1,
+    is_paused_upon_creation= True, 
+    catchup= True,
+    params= {
+        'project_id': PROJECT_ID, 
+        'mudata_raw': PROD_DATASET_MUDATA_RAW,
+        'mudata_curated': PROD_DATASET_MUDATA_CURATED,
+        'env_prefix': 'stg'
+    }
+) as dag:
+
+    task_start_dag= DummyOperator(
+        task_id= 'start_dag'
+    )
+
+    SQL_QUERY_PATH= './queries/get_daily_clients_creation.sql'
+
+    task_query_new_clients_creation= BigQueryInsertJobOperator(
+        task_id= 'query_new_clients_creation',
+        configuration= {
+            'query': {
+                'query': f"{'{%'} include '{SQL_QUERY_PATH}' {'%}'}",
+                'useLegacySql': False,
+                "jobReference": {
+                    "projectId": "{{ params.project_id }}",
+                    "location": 'us-central1'
+                },
+                "destinationTable": {
+                    "projectId": "{{ params.project_id }}",
+                    "datasetId": "{{ params.mudata_curated }}",
+                    "tableId": "look_clients"
+                },
+                "writeDisposition": WriteDisposition.WRITE_APPEND,
+                "createDisposition": CreateDisposition.CREATE_NEVER,
+            }
+        }
+    )
+
+    task_end_dag= DummyOperator(
+        task_id= 'end_dag'
+    )
+
+    task_start_dag >> task_query_new_clients_creation >> task_end_dag
