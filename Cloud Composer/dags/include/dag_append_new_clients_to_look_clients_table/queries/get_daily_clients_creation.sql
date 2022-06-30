dbdcc88d92949e7e7e17107ac05f71f5250bda78
Federico Aragon <federico.aragon@mudafy.com>
2022-06-30 12:04:57 -0300

    query lo fill look_clients

--- Cloud Composer/dags/include/dag_append_new_clients_to_look_clients_table/queries/get_daily_clients_creation.sql
+++ Cloud Composer/dags/include/dag_append_new_clients_to_look_clients_table/queries/get_daily_clients_creation.sql
@@ -1,5 +1,12 @@
 SELECT
-    *
+    clients_creation.client_id                 AS client_id,              		
+    clients_creation.name                      AS name,		            
+    clients_creation.creation_datetime_z       AS creation_datetime_z,		
+    clients_creation.phone                     AS phone,		            
+    clients_creation.email                     AS email,		            
+    clients_creation.country                   AS country,		        
+    false                                      AS is_mail_subscription  
+
 FROM `{{ params.project_id }}.{{ params.mudata_raw }}.clients_creation` clients_creation
 WHERE
         clients_creation.registered_datetime_z >= DATETIME(TIMESTAMP('{{ data_interval_start }}'))
