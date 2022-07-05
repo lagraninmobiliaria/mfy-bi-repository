1aa3ceeadf9f1cbb92ecca21635b2955edb0baff
Federico Aragon <federico.aragon@mudafy.com>
2022-07-05 15:55:46 -0300

    'dag_fill_closed_client_events_table' created with base task .Include the query to get those inferred question events

---
+++ Cloud Composer/dags/include/dag_fill_closed_client_events_table/queries/get_inferred_closed_client_events.sql
@@ -0,0 +1,56 @@
+WITH closed_clients_without_closure AS (
+  SELECT * FROM EXTERNAL_QUERY(
+  "projects/infrastructure-lgi/locations/us-central1/connections/mudafy-prod-replic-us-central", 
+  '''
+    SELECT
+      DISTINCT cp.profile_ptr_id client_id
+    FROM accounts_clientprofile cp
+    WHERE 
+      NOT EXISTS(
+        SELECT
+          ee.client_id 
+        FROM events_event ee
+        WHERE
+              ee.polymorphic_ctype_id= 120 
+          AND ee.client_id = cp.profile_ptr_id
+      ) 
+      AND cp.buying_stage = 'closed'
+    ;'''
+  )
+), closed_clients_last_closed_case AS (
+  SELECT * FROM EXTERNAL_QUERY(
+  "projects/infrastructure-lgi/locations/us-central1/connections/mudafy-prod-replic-us-central", 
+  '''
+    SELECT
+      oc.client_id          client_id,
+      MAX(ee.id)            event_id,
+      MAX(ee.created_at)    created_at
+
+    FROM events_buyingcaseevent ebce
+      LEFT JOIN events_event ee
+        ON ebce.event_ptr_id = ee.id
+      LEFT JOIN accounts_buyinglead bl
+        ON bl.opportunitycase_ptr_id = ee.opportunity_case_id
+      LEFT JOIN accounts_opportunitycase oc
+        ON oc.id = bl.opportunitycase_ptr_id
+      LEFT JOIN accounts_clientprofile cp
+        ON cp.profile_ptr_id= oc.client_id
+      
+    WHERE
+          ebce.kind= 'closed'
+      AND cp.buying_stage= 'closed'
+
+    GROUP BY oc.client_id
+    ;'''
+  )
+)
+SELECT
+  closed_clients_without_closure.client_id,
+  closed_clients_last_closed_case.event_id, 
+  closed_clients_last_closed_case.created_at
+
+FROM closed_clients_without_closure 
+  LEFT JOIN closed_clients_last_closed_case
+    ON closed_clients_last_closed_case.client_id = closed_clients_without_closure.client_id
+ 
+  
---
+++ Cloud Composer/dags/include/dag_fill_closed_client_events_table/stg_fill_closed_client_events.py
@@ -0,0 +1,57 @@
+import os
+
+from datetime import datetime
+
+from dependencies.keys_and_constants import PROJECT_ID, STG_DATASET_MUDATA_RAW
+
+from airflow import DAG
+from airflow.operators.dummy import DummyOperator
+from airflow.operators.python import PythonOperator
+from airflow.providers.google.cloud.operators.bigquery import BigQueryInsertJobOperator
+
+from google.cloud.bigquery import WriteDisposition, CreateDisposition
+
+with DAG(
+    dag_id= 'stg_fill_closed_client_events',
+    start_date= datetime(2021, 1, 1),
+    schedule_interval= None,
+    catchup= False,
+    is_paused_upon_creation= True,
+    tags= ['production'],
+    params= {
+        'project_id': PROJECT_ID,
+        'mudata_raw': STG_DATASET_MUDATA_RAW,
+        'env_prefix': 'stg'
+    }
+) as dag:
+    
+    task_start_dag= DummyOperator(
+        task_id= "start_dag"
+    )
+
+    get_inferred_closed_client_events_query_path= os.path.join(   
+        os.path.dirname(__file__),
+        'queries',
+        'get_inferred_closed_client_events.sql'        
+    )
+
+    with open(get_inferred_closed_client_events_query_path, 'r') as sql_file:
+        get_inferred_closed_client_events_query= sql_file.read()
+
+    task_get_inferred_closed_client_events= BigQueryInsertJobOperator(
+        task_id= 'get_inferred_closed_client_events',
+        configuration= {
+            "query": {
+                "query": get_inferred_closed_client_events_query,
+                "useLegacySql": False
+            }
+        }
+    )
+
+
+    task_end_dag= DummyOperator(
+        task_id= "end_dag"
+    )
+    
+
+    task_start_dag >> task_get_inferred_closed_client_events >> task_end_dag
