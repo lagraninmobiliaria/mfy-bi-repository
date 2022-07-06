7da4e3ddc1ec1bbe6d627776ac340b96df65f417
Federico Aragon <federico.aragon@mudafy.com>
2022-07-06 16:28:47 -0300

    fix: incorrect call for columns

--- Cloud Composer/dags/include/dag_update_fact_clients/queries/get_client_creation_events.sql
+++ Cloud Composer/dags/include/dag_update_fact_clients/queries/get_client_creation_events.sql
@@ -4,5 +4,5 @@ SELECT
 
 FROM `{{ params.project_id }}.{{ params.mudata_raw }}.clients_creation` clients_creation
 WHERE
-        clients_creation.created_datetime_z >= DATETIME(TIMESTAMP("{{ data_interval_start }}"))
-    AND clients_creation.created_datetime_z <  DATETIME(TIMESTAMP("{{ data_interval_end }}"))
+        clients_creation.creation_datetime_z >= DATETIME(TIMESTAMP("{{ data_interval_start }}"))
+    AND clients_creation.creation_datetime_z <  DATETIME(TIMESTAMP("{{ data_interval_end }}"))
