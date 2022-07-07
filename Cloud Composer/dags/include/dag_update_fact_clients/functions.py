c5c1ca9be1db3b9d3e301c7df7f4631d052ee3a1
Federico Aragon <federico.aragon@mudafy.com>
2022-07-07 14:00:25 -0300

    Check the how does the function 'rows_for_new_clients_work'

--- Cloud Composer/dags/include/dag_update_fact_clients/functions.py
+++ Cloud Composer/dags/include/dag_update_fact_clients/functions.py
@@ -1,6 +1,9 @@
+from genericpath import exists
 import os
+from posixpath import dirname
 
 from google.cloud.bigquery import Client
+from grpc import Status
 
 from pandas import DataFrame
 from numpy import nan
@@ -48,7 +51,8 @@ def update_fact_clients_table(**context):
     bq_job_get_client_creation_events= bq_client.get_job(job_id= bq_job_id_get_client_creation_events)
     df_creation_events= bq_job_get_client_creation_events.to_dataframe()
 
-    rows_for_new_clients(df_creation_events= df_creation_events, bq_client= bq_client, **context)
+    creation_clients_rows= rows_for_new_clients(df_creation_events= df_creation_events, bq_client= bq_client, **context)
+    print(creation_clients_rows)
     
     bq_job_id_get_client_reactivation_events= context['task_instance'].xcom_pull('get_client_reactivation_events')
     bq_job_get_client_reactivation_events= bq_client.get_job(job_id= bq_job_id_get_client_reactivation_events)
@@ -65,11 +69,37 @@ def rows_for_new_clients(df_creation_events: DataFrame, bq_client: Client, **con
     for index in range(df_creation_events.shape[0]):
         row= dict(
             client_id= df_creation_events.iloc[index].client_id,
-            from_datetime_z= df_creation_events.iloc[index].creation_datetime_z,
+            from_datetime_z= df_creation_events.iloc[index].creation_datetime_z
+                .to_pydatetime(),
             to_datetime_z= nan,
             is_active= True,
             is_reactive=False,
             last_modified_datetime_z= context['data_interval_start'],
         )
 
-        print(row)
+        check_record_existance_query_path= os.path.join(
+            dirname(__file__),
+            'queries',
+            'client_creation_already_exists.sql'
+        )
+        with open(check_record_existance_query_path, 'r') as sql_file:
+            check_record_existance_query= sql_file.read().format(
+                project_id= context['params'].get('project_id'),
+                dataset_id= context['params'].get('mudata_curated'),
+                table_id= 'fact_clients',
+                field_name= 'client_id',
+                field_value= row.get('client_id')
+            )
+
+        record_exists= bq_client\
+            .query(query= check_record_existance_query)\
+            .result()\
+            .total_rows >= 1
+
+        if not record_exists:
+            rows.append(row)
+    
+    return rows
+    
+
+    
---
+++ Cloud Composer/dags/include/dag_update_fact_clients/queries/client_creation_already_exists.sql
@@ -0,0 +1,6 @@
+SELECT
+    _table.{field_name}
+FROM `{project_id}.{dataset_id}.{table_id}` _table
+WHERE 
+    _table.{field_name} = {field_value}
+    
