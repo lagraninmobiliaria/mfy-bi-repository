b8c3360a0dc8e7425d627bfca09b3ebcc3a7d0f2
Federico Aragon <federico.aragon@mudafy.com>
2022-07-11 16:57:53 -0300

    Fix: Converting first to timestamp and afterwards to datetime

--- Cloud Composer/dags/include/dag_update_fact_clients/queries/update_field_with_closure.sql
+++ Cloud Composer/dags/include/dag_update_fact_clients/queries/update_field_with_closure.sql
@@ -1,7 +1,7 @@
 UPDATE {project_id}.{dataset_id}.fact_clients 
 SET  
     is_active= false,
-    to_datetime_z= DATETIME('{to_datetime_z}')
+    to_datetime_z= DATETIME(TIMESTAMP('{to_datetime_z}'))
 WHERE
         client_id= {client_id}
     AND to_datetime_z IS NULL
