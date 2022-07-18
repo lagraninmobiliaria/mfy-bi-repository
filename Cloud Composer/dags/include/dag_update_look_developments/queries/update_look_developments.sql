a7f2493348476d2509f1e0f96d5586caab6f0c02
Federico Aragon <federico.aragon@mudafy.com>
2022-07-18 12:59:48 -0300

    Changed query to deal with null strings

--- Cloud Composer/dags/include/dag_update_look_developments/queries/update_look_developments.sql
+++ Cloud Composer/dags/include/dag_update_look_developments/queries/update_look_developments.sql
@@ -7,13 +7,19 @@ address                                 AS address,
 latitude,
 longitude,
 location_id,
-status AS development_status,
+status                                  AS development_status,
 broker_agent_id                         AS alliance_id,
 developer_id,
 DATETIME(TIMESTAMP(last_modified_at))   AS update_datetime_z,
-summary.min_price                       AS min_listed_price,
+CASE
+    WHEN summary.min_price != 'null' THEN INTEGER(summary.min_price)
+    ELSE NULL
+END                                     AS min_listed_price,
 currency                                AS min_listed_currency,
-summary.min_price                       AS max_listed_price,
+CASE
+    WHEN summary.max_price != 'null' THEN INTEGER(summary.max_price)
+    ELSE NULL
+END                                     AS max_listed_price,
 currency                                AS max_listed_currency,
 summary                                 AS development_additionals
 
