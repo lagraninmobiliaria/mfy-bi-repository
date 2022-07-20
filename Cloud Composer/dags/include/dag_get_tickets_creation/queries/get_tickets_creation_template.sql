6858e0caec518c8b47f5cf4b8075cd557c220f2a
Federico Aragon <federico.aragon@mudafy.com>
2022-07-20 11:45:01 -0300

    Added EXTERNAL_QUERY

--- Cloud Composer/dags/include/dag_get_tickets_creation/queries/get_tickets_creation_template.sql
+++ Cloud Composer/dags/include/dag_get_tickets_creation/queries/get_tickets_creation_template.sql
@@ -1,11 +1,18 @@
-SELECT
-    TIMESTAMP('{{ data_interval_start }}')      AS registered_datetime_z,
-    tt.id                                       AS ticket_id,
-    tt.client_id                                AS client_id,
-    tt.country                                  AS country_code_iso2,
-    tt.owner_id                                 AS user_id,
-    tt.created_at                               AS created_datetime_z
+SELECT 
+    *
+FROM EXTERNAL_QUERY(
+    "projects/infrastructure-lgi/locations/us-central1/connections/mudafy-prod-replic-us-central",
+    """
+    SELECT
+        TIMESTAMP('{{ data_interval_start }}')      AS registered_datetime_z,
+        tt.id                                       AS ticket_id,
+        tt.client_id                                AS client_id,
+        tt.country                                  AS country_code_iso2,
+        tt.owner_id                                 AS user_id,
+        tt.created_at                               AS created_datetime_z
 
-FROM tickets_ticket tt
-WHERE
-    DATE(tt.created_at) = DATE('{{ data_interval_start.date() }}')
+    FROM tickets_ticket tt
+    WHERE
+        DATE(tt.created_at) = DATE('{{ data_interval_start.date() }}')
+    """
+)
