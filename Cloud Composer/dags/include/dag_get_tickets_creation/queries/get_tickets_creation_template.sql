0b4718e3024c443b49959c645ecad6a2c0b819c6
Federico Aragon <federico.aragon@mudafy.com>
2022-07-20 11:15:41 -0300

    Change in registered_datetime_z field. Converted into timestamp

--- Cloud Composer/dags/include/dag_get_tickets_creation/queries/get_tickets_creation_template.sql
+++ Cloud Composer/dags/include/dag_get_tickets_creation/queries/get_tickets_creation_template.sql
@@ -1,10 +1,10 @@
 SELECT
-    tt.created_at   AS registered_datetime_z,
-    tt.id           AS ticket_id,
-    tt.client_id    AS client_id,
-    tt.country      AS country_code_iso2,
-    tt.owner_id     AS user_id,
-    tt.created_at   AS tt.created_datetime_z
+    TIMESTAMP({{ data_interval_start }})    AS registered_datetime_z,
+    tt.id                                   AS ticket_id,
+    tt.client_id                            AS client_id,
+    tt.country                              AS country_code_iso2,
+    tt.owner_id                             AS user_id,
+    tt.created_at                           AS tt.created_datetime_z
 
 FROM tickets_ticket tt
 WHERE
