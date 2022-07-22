de4a8f215eb903550d45857a7b03178e9aee6b9d
Federico Aragon <federico.aragon@mudafy.com>
2022-07-22 14:46:01 -0300

    Get tickets only if there are opportunity cases

--- Cloud Composer/dags/include/dag_get_buying_opportunity_cases/functions.py
+++ Cloud Composer/dags/include/dag_get_buying_opportunity_cases/functions.py
@@ -35,42 +35,43 @@ def get_ticket_id_for_buying_opportunity_cases(**context):
     stop= perf_counter()
     print(f"Get buying opportunity cases takes: {stop - start:0.4f} seconds")
 
-    client_ids= ','.join([str(client_id) for client_id in df_buying_op_cases.client_id.unique()])
+    if df_buying_op_cases.shape[0]:
+        client_ids= ','.join([str(client_id) for client_id in df_buying_op_cases.client_id.unique()])
 
-    start= perf_counter()
-    get_client_ticket_id_query= queries_manager.get_client_ticket_id_query_template.format(
-        project_id= context['params'].get('project_id'),
-        dataset_id= context['params'].get('mudata_raw'),
-        client_ids= client_ids,
-    )
-    df_tickets_creation= bq_client.query(
-        query= get_client_ticket_id_query
-    )\
-        .to_dataframe()\
-        .set_index(keys= "client_id")
-
-    stop= perf_counter()
-    print(f"Getting ticket_ids takes: {stop - start:0.4f} seconds")
-
-    start= perf_counter()
-    df_buying_op_cases= df_buying_op_cases.join(
-        other= df_tickets_creation, 
-        on= "client_id", 
-        how="left"
-    )
-    stop= perf_counter()
-    print(f"Joining tickets with buying opportunity cases: {stop - start:0.4f} seconds")
-
-    start= perf_counter()
-    bq_client.load_table_from_dataframe(
-        dataframe= df_buying_op_cases,
-        destination= f"{context['params'].get('project_id')}.{context['params'].get('mudata_raw')}.smith_buying_opportunity_cases",
-        job_config= LoadJobConfig(
-            write_disposition= WriteDisposition.WRITE_APPEND,
-            create_disposition= CreateDisposition.CREATE_NEVER
+        start= perf_counter()
+        get_client_ticket_id_query= queries_manager.get_client_ticket_id_query_template.format(
+            project_id= context['params'].get('project_id'),
+            dataset_id= context['params'].get('mudata_raw'),
+            client_ids= client_ids,
         )
-    )
-    stop= perf_counter()
-    print(f"Loading buying opportunity cases with ticket_id: {stop - start:0.4f} seconds")
+        df_tickets_creation= bq_client.query(
+            query= get_client_ticket_id_query
+        )\
+            .to_dataframe()\
+            .set_index(keys= "client_id")
+
+        stop= perf_counter()
+        print(f"Getting ticket_ids takes: {stop - start:0.4f} seconds")
+
+        start= perf_counter()
+        df_buying_op_cases= df_buying_op_cases.join(
+            other= df_tickets_creation, 
+            on= "client_id", 
+            how="left"
+        )
+        stop= perf_counter()
+        print(f"Joining tickets with buying opportunity cases: {stop - start:0.4f} seconds")
+
+        start= perf_counter()
+        bq_client.load_table_from_dataframe(
+            dataframe= df_buying_op_cases,
+            destination= f"{context['params'].get('project_id')}.{context['params'].get('mudata_raw')}.smith_buying_opportunity_cases",
+            job_config= LoadJobConfig(
+                write_disposition= WriteDisposition.WRITE_APPEND,
+                create_disposition= CreateDisposition.CREATE_NEVER
+            )
+        )
+        stop= perf_counter()
+        print(f"Loading buying opportunity cases with ticket_id: {stop - start:0.4f} seconds")
 
 
