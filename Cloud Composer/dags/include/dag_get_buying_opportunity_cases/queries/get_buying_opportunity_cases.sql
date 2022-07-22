ba3d90adce82344e217fbb713e461554bca032e3
Federico Aragon <federico.aragon@mudafy.com>
2022-07-22 12:28:40 -0300

    query corrected

--- Cloud Composer/dags/include/dag_get_buying_opportunity_cases/queries/get_buying_opportunity_cases.sql
+++ Cloud Composer/dags/include/dag_get_buying_opportunity_cases/queries/get_buying_opportunity_cases.sql
@@ -9,12 +9,16 @@ FROM EXTERNAL_QUERY(
             ee.opportunity_case_id      AS opportunity_id,
             ee.client_id                AS client_id,
             ee.prop_id                  AS property_id,
+            ee.development_id           AS development_id, 
+            bl.origin                   AS source,
             ebce.kind                   AS kind,
             ebce.previous_kind          AS previous_kind
 
         FROM events_buyingcaseevent ebce
             LEFT JOIN events_event ee
                 ON ee.id = ebce.event_ptr_id
+            LEFT JOIN accounts_buyinglead bl
+                ON bl.opportunitycase_ptr_id = ee.opportunity_case_id
 
         WHERE
             DATE(ee.created_at) = DATE('{{ data_interval_start.date() }}') 
