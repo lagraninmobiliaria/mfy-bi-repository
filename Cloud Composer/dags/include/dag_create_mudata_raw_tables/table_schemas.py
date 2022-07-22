adb1d640ac0c1a66b8c739a1217ea2afc54aedc3
Federico Aragon <federico.aragon@mudafy.com>
2022-07-22 10:59:03 -0300

    Table schema of smith_search_cases added

--- Cloud Composer/dags/include/dag_create_mudata_raw_tables/table_schemas.py
+++ Cloud Composer/dags/include/dag_create_mudata_raw_tables/table_schemas.py
@@ -292,8 +292,7 @@ SMITH_BUYING_OPPORTUNITY_REACTIVATION_EVENTS= {
         {"name": "event_id", "type": schemaTypes.INTEGER},
         {"name": "opportunity_id", "type": schemaTypes.INTEGER},
         {"name": "created_datetime_z", "type": schemaTypes.TIMESTAMP},
-        {"name": "kind", "type": schemaTypes.STRING},
-        {"name": "previous_kind", "type": schemaTypes.STRING},
+        {"name": "kind", "type": schemaTypes.STRING}
     ],
     "time_partitioning": {
         "field": "created_datetime_z", 
@@ -356,6 +355,7 @@ SMITH_TICKETS_CREATION = {
         {"name": "registered_datetime_z", "type": schemaTypes.DATETIME},
         {"name": "ticket_id", "type": schemaTypes.INTEGER},
         {"name": "client_id", "type": schemaTypes.INTEGER},
+        {"name": "opportunity_id", "type": schemaTypes.INTEGER},
         {"name": "country_code_iso2", "type": schemaTypes.STRING},
         {"name": "user_id", "type": schemaTypes.INTEGER},
         {"name": "created_datetime_z", "type": schemaTypes.DATETIME},
@@ -379,3 +379,41 @@ SMITH_TICKET_REACTIVATION_EVENTS = {
     },
 }
 
+SMITH_SEARCH_CASES= {
+    "table_id": "smith_search_cases",
+    "schema_fields": [
+        {"name": "registered_datetime_z", "type": schemaTypes.TIMESTAMP},
+        {"name": "search_case_id", "type": schemaTypes.INTEGER},
+        {"name": "client_id", "type": schemaTypes.INTEGER},
+        {"name": "ticket_id", "type": schemaTypes.INTEGER},
+        {"name": "urgency", "type": schemaTypes.STRING},
+        {"name": "reason", "type": schemaTypes.STRING},
+        {"name": "financial_status", "type": schemaTypes.STRING},
+        {"name": "credit_status", "type": schemaTypes.STRING},
+        {"name": "credit_notes", "type": schemaTypes.STRING},
+        {"name": "depends_on_sell", "type": schemaTypes.BOOL},
+        {"name": "from_broker", "type": schemaTypes.BOOL},
+        {"name": "budget", "type": schemaTypes.INTEGER},
+        {"name": "budget_currency", "type": schemaTypes.STRING},
+        {"name": "active", "type": schemaTypes.BOOL},
+        {"name": "search_started_datetime_z", "type": schemaTypes.TIMESTAMP},
+        {"name": "filter_search", "type": schemaTypes.STRUCT, "fields": [
+            {"name": "rooms", "type": schemaTypes.INTEGER, "mode": "REPEATED"},
+            {"name": "garages", "type": schemaTypes.INTEGER, "mode": "REPEATED"},
+            {"name": "bedrooms", "type": schemaTypes.INTEGER, "mode": "REPEATED"},
+            {"name": "amenities", "type": schemaTypes.STRING, "mode": "REPEATED"},
+            {"name": "min_price", "type": schemaTypes.INTEGER},
+            {"name": "max_price", "type": schemaTypes.INTEGER},
+            {"name": "currency", "type": schemaTypes.STRING},
+            {"name": "location_id", "type": schemaTypes.INTEGER, "mode": "REPEATED"},
+            {"name": "property_type", "type": schemaTypes.STRING, "mode": "REPEATED"},
+            {"name": "min_total_area", "type": schemaTypes.FLOAT},
+            {"name": "max_total_area", "type": schemaTypes.FLOAT},
+        ]},
+        {"name": "extra_info", "type": schemaTypes.STRING},
+    ],
+    "time_partitioning": {
+        "field": "registered_datetime_z", 
+        "type": TimePartitioningType.DAY 
+    },
+}
